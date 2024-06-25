21afc18bb1dade5abf27ae56a2cf53c2c6edeb54
miniarjabri <123066240+miniarjabri@users.noreply.github.com>
2024-06-25 16:09:44 +0000

    amplify auth

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "myreactappd5a5dd88": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import awsconfig from './aws-exports';
+import { amplifySignOut, withAuthenticator } from '@aws-amplify/ui-react';
+import { Amplify } from 'aws-amplify'; // Correct import
+
+Amplify.configure(awsconfig);
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-<h1>!! verify Amplify !!</h1>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <amplifySignOut /> {/* Use amplifySignOut directly */}
+        <h2>My app content</h2>
       </header>
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
